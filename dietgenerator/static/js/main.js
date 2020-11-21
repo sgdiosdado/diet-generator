4fdb90c55c650764cbfcfe76d628070cb31c7f99
Jesus Lozano <jesus.javi.lozano@hotmail.com>
2020-11-21 13:49:24 -0600

    Added basic BMI and input fields to exclude food

--- dietgenerator/static/js/main.js
+++ dietgenerator/static/js/main.js
@@ -3,3 +3,18 @@ document.addEventListener('DOMContentLoaded', function() {
   let options = {}
   let instances = M.FormSelect.init(elems, options);
 });
+
+function getBMI(){
+  console.log("Hola");
+  //get inputs
+  var height = document.getElementById("Estatura").value;
+  var weight = document.getElementById("Peso").value;
+  height = height / 100;
+
+  var BMI = weight / (height * height);
+  if(BMI == Number.POSITIVE_INFINITY || BMI == 0){
+    document.getElementById("IMCres").value = "IMC";
+  }else{
+    document.getElementById("IMCres").value = BMI;
+  }
+}
