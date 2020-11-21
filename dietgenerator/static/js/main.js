c5d03000e9524d08114cc66a02220c36ffccb772
Sergio Diosdado <sgdc_98@hotmail.com>
2020-11-21 08:03:14 -0600

    Fix multiple select dropdown

---
+++ dietgenerator/static/js/main.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+  let elems = document.querySelectorAll('select');
+  let options = {}
+  let instances = M.FormSelect.init(elems, options);
+});
